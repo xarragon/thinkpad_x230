1367d1101311156e40ac164cddc0c34f168771f8
Martin Persson <xarragon@gmail.com>
2016-02-19 15:39:06 +0100

    acpi: Added LCD backlight control
    Added all Thinkpad X230 events to ACPI script regexp.
    Added LCD backlight control using xbacklight.

--- acpi/actions/tpbtns.sh
+++ acpi/actions/tpbtns.sh
@@ -6,6 +6,8 @@ set -u
 # Alsa control for volume/mute
 ACTL=Master
 
+export DISPLAY=:0
+
 if [ "$2" = 'VOLUP' ]; then
 	amixer set "$ACTL" 5%+ > /dev/null
 elif [ "$2" = 'VOLDN' ]; then
@@ -22,5 +24,11 @@ elif [ "$2" = 'SBTN' ]; then
 	printf "%s\n" 'SBTN'
 elif [ "$2" = 'WLAN' ]; then
 	printf "%s\n" 'WLAN'
+elif [ "$2" = 'BRTDN' ]; then
+	xbacklight -dec 10 -time 0
+elif [ "$2" = 'BRTUP' ]; then
+	xbacklight -inc 10 -time 0
+elif [ "$2" = 'VMOD' ]; then
+	printf "%s\n" 'VMOD'
 fi
 
